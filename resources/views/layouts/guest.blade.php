a2ea927638b681e0b2ba80f16a5ab4ed82aee5c9
Cristofer alejandro payan rodriguez <crispromax00@gmail.com>
2021-12-05 13:04:27 -0500

    https fix and icons login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,7 @@
     <title>{{ config('app.name', 'C.R.I.S') }}</title>
    
     <!-- Fonts -->
+    <link rel="stylesheet" href="{{ asset('css/all.css') }} ">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
